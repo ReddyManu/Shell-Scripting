238bb4458c9455775ab7813aa0b8df866a686b20
Manu Reddy <reddybmanohar@gmail.com>
2021-10-22 22:30:15 +0530

    Variables 2

--- 02-variables.sh
+++ 02-variables.sh
@@ -11,3 +11,12 @@ x=10
 y=20
 
 echo ${x}X${y} = 200
+
+echo Welcome, todays date is 2021-10-20
+
+DATE=$(date +%F)
+echo Hello, todays date is $DATE
+
+ADD=$((4+5+6+5+7*7/2-5))
+
+echo Calculation value = $ADD
