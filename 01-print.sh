6d839f6863b3b4e8fb582ba55e40d573d9976754
Manu Reddy <reddybmanohar@gmail.com>
2021-10-22 18:46:54 +0530

    ESC sequence 3

--- 01-print.sh
+++ 01-print.sh
@@ -7,3 +7,11 @@ echo -e "Testing\nESC sequence"
 
 echo -e "OneTab\tspace"
 echo -e "TwoTabs\t\tspace"
+
+echo -e "\e[31mTestColourRed"
+echo -e "\e[32mTestColourGreen"
+echo -e "\e[33mTestColourYellow"
+echo -e "\e[34mTestColourBlue"
+echo -e "\e[35mTestColourMagenta"
+echo -e "\e[36mTestColourCyan"
+
