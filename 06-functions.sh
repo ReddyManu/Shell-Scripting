0907e57bc6399925593f98fb2e3a949148b06e38
Manu Reddy <reddybmanohar@gmail.com>
2021-10-23 18:23:22 +0530

    Return status Functions 6

--- 06-functions.sh
+++ 06-functions.sh
@@ -5,8 +5,8 @@ function abc() {
   a=10
   echo Value of a in function = $a
   b=200
-  echo First Argument in Function = $1
   return 20
+  echo First Argument in Function = $1
 }
 
 xyz () {
@@ -18,8 +18,8 @@ a=100
 abc $1
 xyz
 b=20
-echo Value of b in main program = $b
 echo The exit status of abc is - $?
+echo Value of b in main program = $b
 echo First Argument in Main Program = $1
 
 
