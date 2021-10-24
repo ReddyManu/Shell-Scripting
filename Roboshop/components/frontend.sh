6fbdf6684e11eb0b989f4fd4b5939988855983b2
Manu Reddy <reddybmanohar@gmail.com>
2021-10-24 13:36:55 +0530

    Frontend 10

--- Roboshop/components/frontend.sh
+++ Roboshop/components/frontend.sh
@@ -5,19 +5,22 @@ Print () {
   echo -n -e "\e[1m$1\e[0m ... "
   echo -e "\n\e[36m ========================== $1 ==========================\e[0m" >>$LOG
 }
-## In previous commit, created above echo command without \n, which did not give space(next line) between Installing, Enabling, and Starting Nginx headings
+## In previous commit, created, above echo command without \n, which did not give space(next line) between Installing, Enabling, and Starting Nginx headings
 
+Stat () {
+  if [ $1 -eq 0 ]
+  then
+    echo -e "\e[1;32mSUCCESS\e[0m"
+  else
+    echo -e "\e[1;31mFAILURE\e[0m"
+  fi
+}
 LOG=/tmp/roboshop.log
 rm -f $LOG
 
 Print "Installing Nginx"
 yum install nginx -y &>>$LOG
-if [ $? -eq 0 ]
-then
-  echo -e "\e[1;32mSUCCESS\e[0m"
-else
-  echo -e "\e[1;31mFAILURE\e[0m"
-fi
+Stat $1
 
 Print "Enabling Nginx"
 systemctl enable nginx
