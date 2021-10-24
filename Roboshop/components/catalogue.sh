e28ab9e2d50f3d2c7e96921a13ebf627aa1095d8
Manu Reddy <reddybmanohar@gmail.com>
2021-10-25 00:23:16 +0530

    Catalogue 5

--- Roboshop/components/catalogue.sh
+++ Roboshop/components/catalogue.sh
@@ -28,11 +28,15 @@ Print "Extract Catalogue"
 unzip -o -d /home/roboshop /tmp/catalogue.zip &>>$LOG
 Stat $?
 
-exit
+Print "Copy content"
+mv /home/roboshop/catalogue-main /home/roboshop/catalogue
+Stat $?
 
-#$ mv catalogue-main catalogue
-#$ cd /home/roboshop/catalogue
-$ npm install
+Print "Install NodeJS dependancies"
+cd /home/roboshop/catalogue
+npm install &>>$LOG
+Stat $?
+
 NOTE: We need to update the IP address of MONGODB Server in systemd.service file
 Now, lets set up the service with systemctl.
 
