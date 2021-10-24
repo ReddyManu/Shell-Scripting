3a462b9bb26943de1b6a6cab936b606b54a51f55
Manu Reddy <reddybmanohar@gmail.com>
2021-10-24 20:16:55 +0530

    create-instance 2

--- Roboshop/components/create-instance.sh
+++ Roboshop/components/create-instance.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-aws ec2 run-instances --image-id ami-0e4e4b2f188e91845 --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
+COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].InstanceId" | wc -l )
+
+if [ $COUNT -eq 0 ]
+then
+  aws ec2 run-instances --image-id ami-0e4e4b2f188e91845 --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
+else
+  echo "Instance already exists"
+fi
+
+# Problem with above command is, if run for 2nd time, it brings up 2nd(ex:cart) instance. Which is not what we want. If Else condition gives a solution.
+
