4e807b57f773019fb18a9d7ce96660b1ea202c67
Manu Reddy <reddybmanohar@gmail.com>
2021-10-24 18:30:32 +0530

    Frontend 21

--- Roboshop/components/frontend.sh
+++ Roboshop/components/frontend.sh
@@ -18,11 +18,11 @@ Print "Download HTML pages"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
 Stat $?
 
-cd /usr/share/nginx/html
-rm -rf *
-unzip /tmp/frontend.zip
-mv frontend-main/* .
-mv static/* .
-rm -rf frontend-master static README.md
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
-systemctl restart nginx
+#cd /usr/share/nginx/html
+#rm -rf *
+#unzip /tmp/frontend.zip
+#mv frontend-main/* .
+#mv static/* .
+#rm -rf frontend-master static README.md
+#mv localhost.conf /etc/nginx/default.d/roboshop.conf
+#systemctl restart nginx
