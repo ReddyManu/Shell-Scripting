4d35765444e1e6fcd47afa9e4972b325bfb5e1da
Manu Reddy <reddybmanohar@gmail.com>
2021-10-25 17:59:03 +0530

    create-instance 6

--- Roboshop/create-instance.sh
+++ Roboshop/create-instance.sh
@@ -7,6 +7,8 @@ if [ $COUNT -eq 0 ]; then
 else
   echo "Instance already exists"
 fi
-
 # Problem with above command (under then in if else condition) is, if run for 2nd time, it brings up 2nd(ex:cart) instance. Which is not what we want. If Else condition gives a solution.
 
+IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null)
+
+sed -e "s/DNSNAME/$1.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
