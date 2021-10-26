e284bde3b994ac07b6f6129f60b9a211ca6d0f4c
Manu Reddy <reddybmanohar@gmail.com>
2021-10-26 17:59:02 +0530

    MySQL 2

--- Roboshop/components/mysql.sh
+++ Roboshop/components/mysql.sh
@@ -11,13 +11,25 @@ Print "Setup $COMPONENT_NAME Repo"
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>$LOG
 Stat $?
 
-Print "Install $COMPONENT_NAME Service"
+Print "Install MariaDB Service"
 yum remove mariadb-libs -y &>>$LOG && yum install mysql-community-server -y &>>$LOG
 Stat $?
 
 Print "Start $COMPONENT_NAME Service"
 systemctl enable mysqld &>>$LOG && systemctl restart mysqld &>>$LOG
 Stat $?
+
+DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
+NEW_PASSWORD="RoboShop@1"
+
+echo 'show databases;' | mysql -uroot -p"${NEW_PASSWORD}"  &>>$LOG
+if [ $? -ne 0 ]; then
+  Print "Changing the Default Password"
+  echo -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${NEW_PASSWORD}';\nuninstall plugin validate_password;" >/tmp/pass.sql
+  mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}" </tmp/pass.sql &>>$LOG
+  Stat $?
+fi
+
 
 exit
 
