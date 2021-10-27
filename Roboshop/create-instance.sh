6991488c30da25644c139066c2640a24e80a554b
Manu Reddy <reddybmanohar@gmail.com>
2021-10-27 18:01:37 +0530

    create-instance 15

--- Roboshop/create-instance.sh
+++ Roboshop/create-instance.sh
@@ -6,7 +6,8 @@ CREATE() {
   if [ $COUNT -eq 0 ]; then
     aws ec2 run-instances --image-id ami-0e4e4b2f188e91845 --instance-type t2.micro --security-group-ids sg-09fb885e47a86a40e --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
   else
-    echo "$1 Instance already exists"
+    echo  -e "\e[1;33m$1 Instance already exists\e[0m"
+    return
   fi
 
   sleep 5
