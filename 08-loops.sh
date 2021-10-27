45be229d1431bd8b02d94824151b0c29b99297b7
Manu Reddy <reddybmanohar@gmail.com>
2021-10-27 12:26:27 +0530

    08-loops 1

---
+++ 08-loops.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# In Loops, there are two major types - while & for
+
+# while loop works on expressions that we used in if statements
+
+a=10
+while [ $a -gt 0 ]
+do
+  echo While Loop
+  a=$(($a-1))
+done
+
